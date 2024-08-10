4eb5bbd38fcf598efa535b95d6202663d9ff2db3
Geek of the Week <94646543+geekotw@users.noreply.github.com>
2024-08-09 21:36:34 -0400

    Create vvvf_calculate.h

---
+++ Core/Inc/vvvf_calculate.h
@@ -0,0 +1,10 @@
+#ifndef INC_VVVF_CALCULATE_H_
+#define INC_VVVF_CALCULATE_H_
+
+void sound_data(void);
+void calculate_2_level (void);
+
+extern const float _Stepped_2WideAlpha [471][2];
+extern const float _Stepped_WideAlpha[636];
+
+#endif /* INC_VVVF_CALCULATE_H_ */
